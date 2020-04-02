7a01be00bda56461a561a71eb1d9548d9b9aaf7a
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-04-02 13:12:02 +0300

    Init

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by MacBook on 02.04.2020.
+//  Copyright © 2020 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
